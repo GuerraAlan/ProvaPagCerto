87c3008eff26abdb8401eda0e94db2d80a3c489f
Alan Guerra <alancosta.unit@hotmail.com>
2019-09-22 18:23:28 -0300

    Criação da estrutura inicial do projeto para prova

---
+++ api/api/Model/EntityModel/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Model.EntityModel
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ api/api/Model/EntityModel/TransactionMap.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Model.EntityModel
+{
+    public static class TransactionMap
+    {
+        public static void Map(this EntityTypeBuilder<Transaction> entity)
+        {
+            entity.ToTable("Table");
+        }
+    }
+}
